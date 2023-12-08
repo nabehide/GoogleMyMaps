225b10244f4f8560e5f20afe209084b666b7e2a2
Mateusz-7 <grzech.mateusz@gmail.com>
2023-12-08 16:06:12 +0100

    Added map link simple validation.

--- GoogleMyMaps/parsers/GoogleMyMapsParser.py
+++ GoogleMyMaps/parsers/GoogleMyMapsParser.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 from pyjsparser import PyJsParser
@@ -7,23 +9,31 @@ class GoogleMyMapsParser:
     def __init__(self):
         self.parser = PyJsParser()
 
-    def get_map_data(self, map_id):
-        raw_data = self._fetch_data(map_id)
+    def get_map_data(self, map_link: str):
+        GoogleMyMapsParser._validate_map_link(map_link)
+        raw_data = GoogleMyMapsParser._fetch_data(map_link)
         parsed_data = self._parse_data(raw_data)
         return parsed_data
 
     @staticmethod
+    def _validate_map_link(map_link: str):
+        map_link_pattern = re.compile(
+            r'https://www\.google\.com/maps/d/u/.*'
+        )
+
+        if not map_link_pattern.match(map_link):
+            raise ValueError('Invalid map link format.')
+
+    @staticmethod
     def _fetch_data(map_link: str):
-        # TODO: map_link validation
-        url = map_link
-        response = requests.get(url)
+        response = requests.get(map_link)
 
         if response.status_code != 200:
             raise Exception(f'Failed to fetch map data. Status code: {response.status_code}')
 
         return response.text
 
-    def _parse_data(self, raw_data):
+    def _parse_data(self, raw_data: str):
         soup = BeautifulSoup(raw_data, 'html.parser')
         script = soup.find_all('script')[1].text
         js = self.parser.parse(script)
