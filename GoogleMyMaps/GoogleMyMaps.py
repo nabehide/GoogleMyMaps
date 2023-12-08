9c32608e09cbd3982a7e68dcd52bd889c9da1aa4
Mateusz-7 <grzech.mateusz@gmail.com>
2023-12-08 16:19:25 +0100

    Update GoogleMyMaps and add icon.

--- GoogleMyMaps/GoogleMyMaps.py
+++ GoogleMyMaps/GoogleMyMaps.py
@@ -9,28 +9,33 @@ class GoogleMyMaps:
     def create_map(self, map_link, chosen_layers: list = None):
         data = self.parser.get_map_data(map_link)
         name = data[2] if len(data) > 2 else 'Unnamed Map'
-        chosen_layers = self._parse_layers(data[6], chosen_layers) if len(data) > 6 else []
+        chosen_layers = GoogleMyMaps._parse_layers(data[6], chosen_layers) if len(data) > 6 else []
         return Map(map_link, name, chosen_layers)
 
-    def _parse_layers(self, layers_data, chosen_layers=None):
+    @staticmethod
+    def _parse_layers(layers_data, chosen_layers=None):
         layers = []
         for index, layer_data in enumerate(layers_data):
             if chosen_layers is None or index in chosen_layers:
                 layer_name = layer_data[2] if len(layer_data) > 2 else f'Unnamed Layer {index + 1}'
-                places = self._parse_places(layer_data[12][0][13][0]) if len(layer_data) > 12 else []
+                places = GoogleMyMaps._parse_places(layer_data[12][0][13]) if len(layer_data) > 12 else []
                 layers.append(Layer(layer_name, places))
         return layers
 
-    def _parse_places(self, places_data):
+    @staticmethod
+    def _parse_places(places_data):
         places = []
-        for place_data in places_data:
-            place_type, coords = self._get_place_type_and_coords(place_data) if len(place_data) > 5 else (None, None)
+        for place_data, place_icon_data in zip(places_data[0], places_data[1]):
+            icon = place_icon_data[0][0] if place_icon_data and len(place_icon_data) > 0 else None
+
+            place_type, coords = GoogleMyMaps._get_place_type_and_coords(place_data) if len(place_data) > 5 else (None, None)
 
             place_info = place_data[5] if len(place_data) > 5 else None
-            name = place_info[0][1][0] if place_info is not None and len(place_info[0]) > 1 else 'Unnamed Place'
+            name = place_info[0][1][0] if place_info and len(place_info[0]) > 1 else 'Unnamed Place'
             photos = [photo[1] for photo in place_info[2]] if len(place_info) > 2 and place_info[2] else None
-            data = self._extract_place_data(place_info) if len(place_info) > 5 else None
-            places.append(Place(name, coords, photos, place_type, data))
+            data = GoogleMyMaps._extract_place_data(place_info)
+
+            places.append(Place(place_type, name, icon, coords, photos, data))
         return places
 
     @staticmethod
@@ -45,9 +50,9 @@ class GoogleMyMaps:
     @staticmethod
     def _extract_place_data(place_info):
         place_data = {}
-        if len(place_info) > 1 and place_info[1] is not None:
+        if len(place_info) > 1 and place_info[1]:
             place_data[place_info[1][0]] = place_info[1][1][place_info[1][2] - 1]
-        if len(place_info) > 3 and place_info[3] is not None:
+        if len(place_info) > 3 and place_info[3]:
             for info in place_info[3]:
                 place_data[info[0]] = info[1][info[2] - 1]
         return place_data if place_data else None
--- GoogleMyMaps/models/Place.py
+++ GoogleMyMaps/models/Place.py
@@ -2,11 +2,13 @@ class Place:
     def __init__(self,
                  place_type: str,
                  name: str,
+                 icon: str or None,
                  coords: list[float] or list[list[float]],
                  photos: list[str] or None,
                  data: dict or None):
         self.place_type = place_type
         self.name = name
+        self.icon = icon
         self.coords = coords
         self.photos = photos
         self.data = data
@@ -20,6 +22,7 @@ class Place:
             if self.data else ''
 
         return f'{self.place_type}: {self.name}\n' \
+               f'      Icon: {self.icon}\n' \
                f'      Coordinates: {self.coords}\n' \
                f'{photos_str}' \
                f'{data_str}'
